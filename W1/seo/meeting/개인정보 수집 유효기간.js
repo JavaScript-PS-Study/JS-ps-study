29681bd0e810ec7f12d50fc97446fab4de1683b7
seo <emma05127@gmail.com>
2023-11-23 21:25:28 +0900

    [Meeting] 개인정보 수집 유효기간 / level 1

--- W1/seo/meeting/개인정보 수집 유효기간.js
+++ W1/seo/meeting/개인정보 수집 유효기간.js
@@ -3,6 +3,28 @@
  * @date 23-11-20
  */
 
+// 두번째 풀이 : @date 23-11-23
+function solution(today, terms, privacies) {
+	var answer = [];
+	const [year, month, day] = today.split('.').map(el => +el);
+	const currentTime = year * 12 * 28 + month * 28 + day;
+
+	const termsHash = terms.reduce((acc, cur) => {
+		acc[cur[0]] = +cur.slice(2) * 28;
+		return acc;
+	}, {});
+
+	privacies.forEach((strDateAndTerm, idx) => {
+		let [thisDate, term] = strDateAndTerm.split(' ');
+		let [thisYear, thisMonth, thisDay] = thisDate.split('.').map(el => +el);
+		const privaciesTime = thisYear * 12 * 28 + thisMonth * 28 + thisDay + termsHash[term];
+		if (privaciesTime <= currentTime) answer.push(idx + 1);
+	});
+
+	return answer;
+}
+
+// 첫번째 풀이 : @date 23-11-20
 function solution(today, terms, privacies) {
 	let answer = [];
 
@@ -30,13 +52,11 @@ function solution(today, terms, privacies) {
 	return answer;
 }
 
-// date - String, 2021.05.02
-// return number[] [year,month,date]
 function addDate(date, addMonth) {
 	let [year, month, day] = date.split('.').map(el => +el);
 
-	month += addMonth; // 11+13 24
-	day--; // 2
+	month += addMonth;
+	day--;
 
 	if (day === 0) {
 		month--;
@@ -44,7 +64,7 @@ function addDate(date, addMonth) {
 	}
 
 	if (month > 12) {
-		year += Math.floor(month / 12); // 2010
+		year += Math.floor(month / 12);
 		month %= 12;
 	}
 
@@ -55,12 +75,3 @@ function addDate(date, addMonth) {
 
 	return [year, month, day];
 }
-
-// Date 객체를 이용해 비교할 수 있음
-// 1. today를 Date로 parsing 한 값을 today에 저장한다
-// 2. terms를 hash화 한다.
-// 2. privacies를 순회하며 각 요소 privacie 문자열 처리를 한다.
-//  2-1. privacie를 공백 기준으로 자른다 [가입날짜, term] 형식
-//  2-3. '가입날짜'를 Date 객체를 이용해 parsing 해 privacie 변수에 할당한다.
-//  2-4. privacie 변수에 terms[term]*28 을 더한다.
-//  2-5. today와 비교한다. 만약 today 보다 해당 날짜가 크다면 answer.push() 한다.
