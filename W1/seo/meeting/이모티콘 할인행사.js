9ccc48e7e1eb75e5bedfd4c9cea9ad3dae06ca0c
seo <emma05127@gmail.com>
2023-11-23 21:25:32 +0900

    [Meeting] 이모티콘 할인행사 / level 2

--- W1/seo/meeting/이모티콘 할인행사.js
+++ W1/seo/meeting/이모티콘 할인행사.js
@@ -7,39 +7,45 @@ function solution(users, emoticons) {
 	var answer = [];
 	const discounts = [10, 20, 30, 40];
 	dfs(emoticons.length, []);
-	answer.sort((a, b) => b.emoPlus - a.emoPlus || b.sales - a.sales);
-	return [answer[0].emoPlus, answer[0].sales];
+	// answer.sort((a, b) => b.emoPlus - a.emoPlus || b.sales - a.sales);
+	return [answer.emoPlus, answer.sales];
 
 	function dfs(depth, discountPer) {
 		if (discountPer.length >= emoticons.length) {
 			const applyDiscounts = emoticons.map((cur, idx) => cur * (1 - discountPer[idx] * 0.01));
 
-			answer.push(
-				users.reduce(
-					(acc, [userPercent, budget]) => {
-						let userSales = 0;
+			const calculateSales = users.reduce(
+				(acc, [userPercent, budget]) => {
+					let userSales = 0;
 
-						for (let idx = 0; idx < emoticons.length; idx++) {
-							const applyDiscount = applyDiscounts[idx];
+					for (let idx = 0; idx < emoticons.length; idx++) {
+						const applyDiscount = applyDiscounts[idx];
 
-							if (userPercent <= discountPer[idx]) {
-								budget -= applyDiscount;
+						if (userPercent <= discountPer[idx]) {
+							budget -= applyDiscount;
 
-								if (budget <= 0) {
-									acc.emoPlus++;
-									userSales = 0;
-									break;
-								} else {
-									userSales += applyDiscount;
-								}
+							if (budget <= 0) {
+								acc.emoPlus++;
+								userSales = 0;
+								break;
+							} else {
+								userSales += applyDiscount;
 							}
 						}
-						acc.sales += userSales;
-						return acc;
-					},
-					{ emoPlus: 0, sales: 0 },
-				),
+					}
+					acc.sales += userSales;
+					return acc;
+				},
+				{ emoPlus: 0, sales: 0 },
 			);
+
+			if (answer.emoPlus === undefined) {
+				answer = calculateSales;
+			} else if (answer.emoPlus && answer.emoPlus < calculateSales.emoPlus) {
+				answer = calculateSales;
+			} else if (answer.emoPlus && answer.emoPlus === calculateSales.emoPlus) {
+				answer = answer.sales < calculateSales.sales ? calculateSales : answer;
+			}
 
 			return;
 		}
