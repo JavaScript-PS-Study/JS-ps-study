531d12d9b0071ee974131cf42c9f40918e648cf2
seo <emma05127@gmail.com>
2023-11-24 16:48:31 +0900

    [Meeting] 개인정보 수집 유효기간 / level 1
    - #1 대면 스터디 피드백 적용

--- W1/seo/meeting/개인정보 수집 유효기간.js
+++ W1/seo/meeting/개인정보 수집 유효기간.js
@@ -4,20 +4,18 @@
  */
 
 // 두번째 풀이 : @date 23-11-23
+// 세번째 풀이 : @date 23-11-24 - 대면 스터디 피드백 반영. Map 적용
 function solution(today, terms, privacies) {
 	var answer = [];
 	const [year, month, day] = today.split('.').map(el => +el);
 	const currentTime = year * 12 * 28 + month * 28 + day;
 
-	const termsHash = terms.reduce((acc, cur) => {
-		acc[cur[0]] = +cur.slice(2) * 28;
-		return acc;
-	}, {});
+	const termsHash = terms.reduce((acc, cur) => acc.set(cur[0], +cur.slice(2) * 28), new Map());
 
 	privacies.forEach((strDateAndTerm, idx) => {
-		let [thisDate, term] = strDateAndTerm.split(' ');
+		let [thisDate, thisTerm] = strDateAndTerm.split(' ');
 		let [thisYear, thisMonth, thisDay] = thisDate.split('.').map(el => +el);
-		const privaciesTime = thisYear * 12 * 28 + thisMonth * 28 + thisDay + termsHash[term];
+		const privaciesTime = thisYear * 12 * 28 + thisMonth * 28 + thisDay + termsHash.get(thisTerm);
 		if (privaciesTime <= currentTime) answer.push(idx + 1);
 	});
 
@@ -30,14 +28,11 @@ function solution(today, terms, privacies) {
 
 	today = today.split('.').map(el => +el);
 
-	terms = terms.reduce((acc, cur) => {
-		acc[cur[0]] = +cur.slice(2);
-		return acc;
-	}, {});
+	terms = terms.reduce((acc, cur) => acc.set(cur[0], +cur.slice(2)), new Map());
 
 	privacies.forEach((cur, idx) => {
 		let [privacyDate, termType] = cur.split(' ');
-		privacyDate = addDate(privacyDate, terms[termType]);
+		privacyDate = addDate(privacyDate, terms.get(termType));
 
 		if (today[0] > privacyDate[0]) answer.push(idx + 1);
 		else if (today[0] === privacyDate[0] && today[1] > privacyDate[1]) answer.push(idx + 1);
