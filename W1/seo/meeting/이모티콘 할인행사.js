5e94bac9fa55f0f454cdb178204e3b100dd808f3
seo <emma05127@gmail.com>
2023-11-24 21:07:59 +0900

    [Meeting] 이모티콘 할인행사 / level 2
    - #1 PR 피드백 반영
    - if-else문 제거, 미사용 dfs함수의 depth 인자 제거
    - 변수명 명확히 수정, 설명 주석 추가

--- W1/seo/meeting/이모티콘 할인행사.js
+++ W1/seo/meeting/이모티콘 할인행사.js
@@ -6,31 +6,38 @@
 function solution(users, emoticons) {
 	var answer = [];
 	const discounts = [10, 20, 30, 40];
-	dfs(emoticons.length, []);
-	// answer.sort((a, b) => b.emoPlus - a.emoPlus || b.sales - a.sales);
+	dfs();
+
 	return [answer.emoPlus, answer.sales];
 
-	function dfs(depth, discountPer) {
-		if (discountPer.length >= emoticons.length) {
-			const applyDiscounts = emoticons.map((cur, idx) => cur * (1 - discountPer[idx] * 0.01));
+	// sort 와 if문의 효율성 차이를 비교하기 위해 주석 처리
+	// answer.sort((a, b) => b.emoPlus - a.emoPlus || b.sales - a.sales);
+	// return [answer[0].emoPlus, answer[0].sales];
+
+	function dfs(discountPermutation = []) {
+		if (discountPermutation.length === emoticons.length) {
+			const emojiAppliedDiscounts = emoticons.map(
+				(cur, idx) => cur * (1 - discountPermutation[idx] * 0.01),
+			);
 
 			const calculateSales = users.reduce(
 				(acc, [userPercent, budget]) => {
 					let userSales = 0;
 
 					for (let idx = 0; idx < emoticons.length; idx++) {
-						const applyDiscount = applyDiscounts[idx];
+						const emojiAppliedDiscount = emojiAppliedDiscounts[idx];
 
-						if (userPercent <= discountPer[idx]) {
-							budget -= applyDiscount;
+						// 유저가 원하는 할인율보다 조합된 할인이 클 경우
+						if (userPercent <= discountPermutation[idx]) {
+							budget -= emojiAppliedDiscount;
 
 							if (budget <= 0) {
 								acc.emoPlus++;
 								userSales = 0;
 								break;
-							} else {
-								userSales += applyDiscount;
 							}
+
+							if (budget > 0) userSales += emojiAppliedDiscount;
 						}
 					}
 					acc.sales += userSales;
@@ -39,19 +46,29 @@ function solution(users, emoticons) {
 				{ emoPlus: 0, sales: 0 },
 			);
 
+			// 초기 값 판별
 			if (answer.emoPlus === undefined) {
 				answer = calculateSales;
-			} else if (answer.emoPlus && answer.emoPlus < calculateSales.emoPlus) {
+				return;
+			}
+
+			if (answer.emoPlus < calculateSales.emoPlus) {
 				answer = calculateSales;
-			} else if (answer.emoPlus && answer.emoPlus === calculateSales.emoPlus) {
+				return;
+			}
+
+			// 우선순위가 높은 emoPlus의 구매 횟수가 같은 경우
+			if (answer.emoPlus === calculateSales.emoPlus) {
+				// 매출액 기준으로 높은 요소를 answer에 할당한다.
 				answer = answer.sales < calculateSales.sales ? calculateSales : answer;
+				return;
 			}
 
 			return;
 		}
 
 		for (let i = 0; i < 4; i++) {
-			dfs(depth - 1, [...discountPer, discounts[i]]);
+			dfs([...discountPermutation, discounts[i]]);
 		}
 	}
 }
