66418b5f8ca382d9bf603b4cd03772e814f08b1f
minh0518 <rubics@naver.com>
2023-11-27 23:53:52 +0900

    [Daily] 주차 요금 계산 / level 2
    주석을 추가 합니다

--- W2/Austin/daily/주차 요금 계산.js
+++ W2/Austin/daily/주차 요금 계산.js
@@ -15,24 +15,32 @@ function solution(fees, records) {
 		const [time, carNum, inOut] = record.split(' ');
 		const convertedTime = convertTime(time);
 
+		// 차번호:[시간1,시간2,...]
+		// IN,OUT 여부와 관계 없이 시간들을 추가
 		info.set(carNum, info.has(carNum) ? [...info.get(carNum), convertedTime] : [convertedTime]);
 	});
 
 	for (let [carNum, value] of info) {
 		const calcTimeArr = [];
 
+		// 시간은 항상 입,출 방식이므로 2자리씩 끊어가며 계산
 		let index = 0;
 		while (index < value.length - 1) {
 			const [inTime, ouTime] = value.slice(index, index + 2);
 			calcTimeArr.push(ouTime - inTime);
 			index += 2;
 		}
+
+		// 마지막 출차시간이 없으면 23:59 기준으로 계산
 		if (value.length % 2 === 1) calcTimeArr.push(LAST_TIME - value.pop());
 
+		// 전체 이용시간 합산
 		const totalUseTime = calcTimeArr.reduce((a, b) => a + b);
 
+		// 최소 기본요금
 		const totalBasicPrice = fees[1];
 
+		// 최종 계산
 		const totalExtraPrice =
 			totalUseTime > fees[0] ? Math.ceil((totalUseTime - fees[0]) / fees[2]) * fees[3] : 0;
 
