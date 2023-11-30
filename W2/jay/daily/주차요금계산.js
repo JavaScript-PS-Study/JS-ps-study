f3ab18f2ad3ecd113a4a9ca1b855eea9603374d7
g2hhh2ee <g2hhh2ee@gmail.com>
2023-12-01 02:29:12 +0900

    [Daily] 주차 요금 계산 / level 2

---
+++ W2/jay/daily/주차요금계산.js
@@ -0,0 +1,15 @@
+/*
+ * @url https://school.programmers.co.kr/learn/courses/30/lessons/92341
+ * @date 23-11-29
+ */
+
+function solution(fees, records) {
+    let answer = [];
+    const cars_log = [];
+    const cars_pay = [];
+    records.forEach((record) => {
+        let [time, car, IO] = record.split(' ');
+        let [hour, minute] = time.split(':').map(Number);
+    });
+    return answer;
+}
